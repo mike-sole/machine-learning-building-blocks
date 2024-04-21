00dff55023626fa3f48cccee599bbde7bc260a45
Mike Sole <msole@certinia.com>
2024-04-21 15:27:02 +0100

    add script to run local environment in docker

---
+++ run-local-dev.sh
@@ -0,0 +1,7 @@
+export JEKYLL_VERSION=4.2.2
+
+docker run --rm \
+  --volume=$(pwd):/srv/jekyll \
+  -p 4000:4000 \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  /bin/bash -c "gem install webrick && jekyll serve --livereload"
